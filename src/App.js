291b69be95b5cf76bdf743a362cfad2e87efd641
kanisorn <kanisorn.peach@gmail.com>
2019-01-25 02:04:26 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React, { Component } from "react";
-import styled from "styled-components";
-import { Flex, Button, Box, Text } from "rebass";
-import { PageContainer, Color, Highlight } from "./styleguides";
+// import styled from "styled-components";
+import { Flex, Button, Box, Text, Image } from "rebass";
+import { Highlight } from "../src/styleguides";
+
+import ImgPart1 from "../src/assets/part1.png";
+import ImgPart2 from "../src/assets/part2.png";
+import ImgPart3 from "../src/assets/part3.jpg";
 
 class App extends Component {
   constructor() {
@@ -16,14 +20,26 @@ class App extends Component {
   render() {
     return (
       <Flex justifyContent="center" flexDirection="column" alignItems="center">
-        <Box py={5}>
-          <Text fontSize={30} fontWeight="bold" color={"black"}>
-            Instruction
-          </Text>
-        </Box>
+        <Text py={4} fontSize={30} fontWeight="bold" color={"black"}>
+          Instruction
+        </Text>
+        <Text fontSize={18} py={2} px={3} textAlign="center">
+          1. Install <Highlight>CoinBase Wallet</Highlight> on PlayStore/Google
+          Play.
+        </Text>
+        <Image width={[1 / 3]} src={ImgPart1} my={2} />
+        <Text fontSize={18} py={2} px={3} textAlign="center">
+          2. Open <Highlight>Dapps</Highlight> Tab on Application.
+        </Text>
+        <Image width={[1 / 2]} src={ImgPart2} my={2} />
+        <Text fontSize={18} py={3} px={3} textAlign="center">
+          3. Insert <Highlight>URL</Highlight> which you will receive from
+          staffs.
+        </Text>
+        <Image width={[1 / 2]} src={ImgPart3} my={1} />
         <Box py={3}>
-          <Button onClick={this.openCoinBaseLink} width={200}>
-            Open CoinBase Wallet
+          <Button onClick={this.openCoinBaseLink} width={160}>
+            OK Let's claim!
           </Button>
         </Box>
       </Flex>
